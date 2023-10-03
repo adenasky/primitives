59f41505d3b4bc92ee0e2ffd7a318b4f6081000d
adenasky <adenasky@gmail.com>
2023-10-03 20:26:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 17515;
+        int costPerMile = 20;
+
+        int miles = ticketCost / costPerMile;
+        System.out.println("Начислено миль: " + miles);
+    }
+}
